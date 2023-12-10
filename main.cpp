e826d53bd391dad221269a77cc2177303b727fd0
hosein12a <153413720+hosein12a@users.noreply.github.com>
2023-12-09 21:36:55 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+ main() {
+ float a,b,c,d,e,f;
+ cout<<"please enter a=";
+ cin>>a;
+ cout<<"please enter b=";
+ cin>>b;
+    c=a+b;
+cout<<"result of + is= "<<c;
+    d=a*b;
+cout<<"result of * is= "<<d;
+    e=a-b;
+cout<<"result of - is="<<e;
+    f=a/b;
+    cout<<"result of % is="<<f;
+}
